7d2f893d18deb91d63a451694ef1afc21b1de35c
zgourareda <redazr1997@gmail.com>
2023-12-27 15:57:15 +0100

    project-setup

---
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -0,0 +1,54 @@
+
+package com.example.android.eggtimernotifications.ui
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.eggtimernotifications.R
+import com.example.android.eggtimernotifications.databinding.FragmentEggTimerBinding
+import com.google.firebase.messaging.FirebaseMessaging
+
+class EggTimerFragment : Fragment() {
+
+    private val TOPIC = "breakfast"
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: FragmentEggTimerBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_egg_timer, container, false
+        )
+
+        val viewModel = ViewModelProvider(this).get(EggTimerViewModel::class.java)
+
+        binding.eggTimerViewModel = viewModel
+        binding.lifecycleOwner = this.viewLifecycleOwner
+
+        // TODO: Step 1.7 call create channel
+
+        return binding.root
+    }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        // TODO: Step 1.6 START create a channel
+
+        // TODO: Step 1.6 END create a channel
+
+    }
+
+    companion object {
+        fun newInstance() = EggTimerFragment()
+    }
+}
+
