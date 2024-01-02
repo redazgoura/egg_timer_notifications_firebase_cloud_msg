cf69aebac9da4fa7e517e6240110ce0197e9e324
zgourareda <redazr1997@gmail.com>
2024-01-02 20:44:40 +0100

    send-messages-using-FCM

---
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -0,0 +1,27 @@
+package com.example.android.eggtimernotifications
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    /**
+     * called when message is received */
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String?) {
+
+        Log.d("Token", "Refreshed Token : $token")
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+
+    }
+
+
+
+}
